64f8b4f969fa2f9cef74afea105d849ce525b869
imaksimSuhinin <maxsic1985@gmail.com>
2023-05-19 16:05:13 +0300

    view

---
+++ Assets/Code/Views/PlayerBoxColliderView.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace MSuhininTestovoe.B2B
+{
+    public sealed class PlayerBoxColliderView : BaseView
+
+    {
+        [SerializeField] private BoxCollider _boxCollider;
+
+        public BoxCollider BoxCollider => _boxCollider;
+    }
+}
